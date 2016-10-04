a7e8eca4f61c7d1ef0f8d560911131413a8fc7b4
Elliot Lewis <lewell94@gmail.com>
2016-10-04 14:13:23 +0100

    Change reading of input to read multiple digit inputs
    Use a scanner instead of bufio.NewReader to allow reading of multiple
    digit numbers

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
 	"time"
 	"os"
+	"text/scanner"
+	"strconv"
 )
 
 func main() {
@@ -16,20 +17,31 @@ func main() {
 	rand        := rand.New(source)
 	magicNumber := rand.Intn(10)
 
-	// Create a new reader, reading from Stdin
-	reader := bufio.NewReader(os.Stdin)
-
 	// Tell the user what they have to do
-	fmt.Println("Try and guess the magic number between 1 and 10...")
+	fmt.Println("Try and guess the magic number between 0 and 10 (inclusive)...")
 
-	// Read the user's input from the reader, convert from int32 rune to int character
-	input, _, _ := reader.ReadRune()
-	guess       := int(input - '0')
+	// Create scanner, initialising it to read from Stdin
+	var s scanner.Scanner
+	s.Init(os.Stdin)
 
-	// Check to see whether the user has guessed correctly
-	if guess == magicNumber {
-		fmt.Println("You are correct! Congratulations.")
-	} else {
-		fmt.Printf("%s %d%s \n", "You are incorrect! The magic number was", magicNumber, ".")
+	// Scan the inputted text
+	var token rune
+	for token != scanner.EOF {
+
+		// Get the rune of the inputted text
+		token = s.Scan()
+
+		// Get the inputted text (as string), convert it to an int
+		text     := s.TokenText()
+		guess, _ := strconv.Atoi(text)
+
+		// Check to see whether the user has guessed correctly
+		if guess == magicNumber {
+			fmt.Println("You are correct! Congratulations.")
+		} else {
+			fmt.Printf("%s %d%s \n", "You are incorrect! The magic number was", magicNumber, ".")
+		}
+
+		os.Exit(0)
 	}
 }
